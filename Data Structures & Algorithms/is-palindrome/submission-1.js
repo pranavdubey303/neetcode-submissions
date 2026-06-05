cef26c562bdc25b6e39605dd332e917f1246daa0
pranavdubey303 <69785102+pranavdubey303@users.noreply.github.com>
2026-06-06 00:59:26 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+
+const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+
+    if(cleaned == '') {
+        return true;
+    }
+
+
+    let i = 0;
+    let j = cleaned.length - 1;
+
+    while ( i < j) {
+        if(cleaned[i] !== cleaned[j]) {
+            return false;
+        } 
+
+        i++;
+        j--;
+    }
+
+    return true;
+
+
+
+    }
+}

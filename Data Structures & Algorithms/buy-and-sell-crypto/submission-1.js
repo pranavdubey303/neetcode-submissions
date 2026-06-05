729c5db7787c5aa75a3818607883c4d4e3b6f67b
pranavdubey303 <69785102+pranavdubey303@users.noreply.github.com>
2026-06-06 01:19:30 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+
+        let maxProfit = 0;
+
+        let minPrice = prices[0];
+
+        for(let i = 1 ; i < prices.length ; i++) {
+
+            minPrice = Math.min(minPrice , prices[i]);
+
+            let profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit , profit);
+        }
+
+        return maxProfit;
+    }
+}

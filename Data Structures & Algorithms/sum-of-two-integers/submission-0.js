a83f8701d861ec70697e84b240b5a88b3f803a14
pranavdubey303 <69785102+pranavdubey303@users.noreply.github.com>
2026-06-09 00:00:08 +0530

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a, b) {
+
+
+        while (b !== 0) {
+            let carry = (a & b) << 1;
+
+             a = a ^ b;
+
+             b = carry;
+        }
+
+
+        return a;
+    }
+}
